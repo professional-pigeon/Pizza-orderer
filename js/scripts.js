8a8ad9ed953513ec986a6a1916f580a74e36920a
professional-pigeon <kyle.kayperez@gmail.com>
2021-06-18 09:28:39 -0700

    create function to add pizza details to a pizza object.

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@
 // Pizza logic
 
 function Pizza() {
-  this.toppings = {};
+  this.details = {};
 }
 
-let Pizza1 = new Pizza();
+let pizza1 = new Pizza();
 
 function PizzaParts(topping1, topping2, topping3, size) {
   this.topping1 = topping1;
@@ -17,3 +17,19 @@ function PizzaParts(topping1, topping2, topping3, size) {
 
 let yourPizza = new PizzaParts("Red Onion", "Peppers", "Sausage", "Large")
 
+Pizza.prototype.addPizzaParts = function(PizzaParts) {
+  this.details[PizzaParts.topping1] = PizzaParts;
+}
+
+pizza1.toppings[0].topping1
+pizza1.toppings[0].topping2
+pizza1.toppings[0].topping3
+pizza1.toppings[0].size
+pizza1.toppings.push(yourPizza) (this works)
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts[contact.firstName] = contact;
+};
+
+pizza1.details["Red Onion"].topping1
+or pizza1.details[key].topping1
